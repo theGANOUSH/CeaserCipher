977574206b024418ccdcba888a32b0c0d000c184
theGANOUSH <babaganoush45@gmail.com>
2015-04-17 13:49:34 -0400

    Added Crack() Method

--- CeaserCipher/Program.cs
+++ CeaserCipher/Program.cs
@@ -8,8 +8,8 @@ namespace CeaserCipher
 {
     class Program
     {
-        static sealed double[] table = {8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.1, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1};
-        static sealed String list = "abcdefghijklmnopqrstuvwxyz";
+        static double[] table = {8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.1, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1};
+        static String list = "abcdefghijklmnopqrstuvwxyz";
 
         static int let2num (char c)
         {
@@ -104,6 +104,78 @@ namespace CeaserCipher
 
         }
         
+        static double[] rotate(int n, double[] lFreqs)
+        {
+            int offset = (lFreqs.Length - n) % lFreqs.Length;
+            int j;
+
+            if(offset > 0)
+            {
+                double[] clone = (double[])lFreqs.Clone();
+
+                for(int i = 0; i < lFreqs.Length - 1; i++)
+                {
+                    j = (i + (lFreqs.Length - offset)) % lFreqs.Length;
+                    lFreqs[i] = clone[i];
+                }
+                
+            }
+            return lFreqs;
+        }
+
+        static double chiSqr(double[] of)
+        {
+            double sum = 0;
+            for(int i = 0; i < of.Length; i++)
+            {
+                sum += Math.Pow((of[i] - table[i]), 2) / table[i];
+            }
+
+            return sum;
+        }
+        
+        static int position(double a, double[] chiSqrList)
+        {
+            int index = 0;
+            for(int i = 0; i < chiSqrList.Length; i++)
+            {
+                if (a == chiSqrList[i])
+                {
+                    index = i;
+                }
+            }
+
+            return index;
+
+        }
+
+        static String crack(string str)
+        {
+            double[] encodedFreq = freqs(str);
+            double[] encodedChiSqr = new double[26];
+
+            String plainText;
+
+            for(int i = 0; i < encodedChiSqr.Length; i++)
+            {
+                encodedChiSqr[i] = chiSqr(encodedFreq);
+                encodedFreq = rotate(1, encodedFreq);
+            }
+            double lowest = encodedChiSqr[0];
+
+            for(int j = 1; j < encodedChiSqr.Length; j++)
+            {
+                if(encodedChiSqr[j] < lowest)
+                {
+                    lowest = encodedChiSqr[j];
+                }
+            }
+
+            plainText = decode(position(lowest, encodedChiSqr), str);
+
+            return plainText;
+        }
+
         
         static void Main(string[] args)
         {
@@ -111,7 +183,7 @@ namespace CeaserCipher
             Console.WriteLine(num2let(25));
             Console.WriteLine(shift(3, 'n'));
             Console.WriteLine(encode(3, "Haskellisfun!"));
-
+            Console.WriteLine(crack("myxqbkdevkdsyxc yx mywzvodsxq dro ohkw!"));
 
 
 
